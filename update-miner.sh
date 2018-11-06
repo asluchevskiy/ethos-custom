94c9cf4e3c79a1728b9d0aa10369ef2234117e31
Arseniy Sluchevskiy <arseniy.sluchevskiy@gmail.com>
2018-11-06 16:57:42 +0300

    Update update-miner.sh

--- update-miner.sh
+++ update-miner.sh
@@ -2,5 +2,6 @@ TMP_DIR=/tmp/ethos-t-rex
 mkdir $TMP_DIR
 git -C $TMP_DIR clone https://github.com/asluchevskiy/ethos-t-rex.git
 sudo cp -r $TMP_DIR/ethos-t-rex/opt/miners/* /opt/miners/
+sudo service ethos-miner-monitor restart
 minestop
 rm -rf $TMP_DIR
