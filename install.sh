a5be56aa38bacd5e2c8169f5a30dc7adeba1a228
unrealjke <35392833+unrealjke@users.noreply.github.com>
2018-12-24 23:59:00 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ TMP_DIR=/tmp
 mkdir $TMP_DIR
 git -C $TMP_DIR clone https://github.com/unrealjke/ethos-custom.git
 sudo cp -r $TMP_DIR/ethos-custom/opt/* /opt/
+sudo chown 1000:1000 /opt/miners/custom/*
 sudo kill `ps ax | grep /opt/ethos/sbin/ethos-miner-daemon | grep -v grep | awk {'print $1'}`
 rm -rf $TMP_DIR/ethos-custom
