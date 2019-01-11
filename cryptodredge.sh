bfa98c7e4d77d5fb2f36917fb3a46118ea1e9db2
unrealjke <35392833+unrealjke@users.noreply.github.com>
2019-01-11 21:11:30 +0400

    Update cryptodredge.sh

--- cryptodredge.sh
+++ cryptodredge.sh
@@ -6,13 +6,13 @@ echo 'waiting for 15 seconds before continue'
 sleep 15
 curl -s https://raw.githubusercontent.com/unrealjke/ethos-custom/master/opt/miners/custom/hash-monitor > /opt/miners/cu$
 cd /tmp
-wget https://github.com/technobyl/CryptoDredge/releases/download/v$VERSION/CryptoDredge_$VERSION\_cuda_10.0_linux.tar.gz
+wget -q https://github.com/technobyl/CryptoDredge/releases/download/v$VERSION/CryptoDredge_$VERSION\_cuda_10.0_linux.tar.gz
 rm -rf /opt/miners/custom/cryptodredge
 mkdir /opt/miners/custom/cryptodredge
 tar -xzf CryptoDredge_$VERSION\_cuda_10.0_linux.tar.gz
 rm CryptoDredge_$VERSION\_cuda_10.0_linux.tar.gz
 mv ./CryptoDredge_$VERSION/* /opt/miners/custom/cryptodredge
-rm -rf ./CryptoDredge_'$VERSION'
+rm -rf ./CryptoDredge*
 rm /opt/miners/custom/custom
 ln -s /opt/miners/custom/cryptodredge/CryptoDredge /opt/miners/custom/custom
 echo 'custom cryptodredge-'$VERSION > /opt/miners/custom/custom-version.txt
