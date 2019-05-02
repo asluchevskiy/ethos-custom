5dbc09c7bc7887e2e159957d3ca445acfd7b7836
unrealjke <35392833+unrealjke@users.noreply.github.com>
2019-05-02 08:44:22 +0400

    Update gminer.sh

--- gminer.sh
+++ gminer.sh
@@ -5,11 +5,11 @@ echo 'waiting for 15 seconds before continue'
 sleep 15
 curl -s https://raw.githubusercontent.com/unrealjke/ethos-custom/master/opt/miners/custom/hash-monitor > /opt/miners/custom/hash-monitor
 cd /tmp
-wget -q  https://github.com/develsoftware/GMinerRelease/releases/download/1.40/gminer_gminer_$VERSION_linux64.tar.xz
+wget -q https://github.com/develsoftware/GMinerRelease/releases/download/1.40/gminer_$VERSION\_linux64.tar.xz
 rm /opt/miners/custom/custom
 rm -rf /opt/miners/custom/gminer
 mkdir /opt/miners/custom/gminer
-tar -xvf gminer_$VERSION\_minimal_linux64.tar.gz -C /opt/miners/custom/gminer
+tar -xvf gminer_$VERSION\_linux64.tar.xz -C /opt/miners/custom/gminer
 rm -rf gminer_*
 ln -s /opt/miners/custom/gminer/miner /opt/miners/custom/custom
 echo 'custom gminer-'$VERSION > /opt/miners/custom/custom-version.txt
