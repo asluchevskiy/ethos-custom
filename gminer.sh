5df04875b3f8deca047fabab68047e0158e5906a
unrealjke <35392833+unrealjke@users.noreply.github.com>
2019-05-21 21:46:32 +0400

    Update gminer.sh

--- gminer.sh
+++ gminer.sh
@@ -1,12 +1,12 @@
-VERSION='1_41'
-VER='1.41'
+VERSION='1_43'
+VER='1.43'
 disallow
 minestop
 echo 'waiting for 15 seconds before continue'
 sleep 15
 curl -s https://raw.githubusercontent.com/unrealjke/ethos-custom/master/opt/miners/custom/hash-monitor > /opt/miners/custom/hash-monitor
 cd /tmp
-wget -q https://github.com/develsoftware/GMinerRelease/releases/download/$VER/gminer_$VERSION\_linux64.tar.xz
+wget -q https://github.com/develsoftware/GMinerRelease/releases/download/$VER/gmine_$VERSION\_linux64.tar.xz
 rm /opt/miners/custom/custom
 rm -rf /opt/miners/custom/gminer
 mkdir /opt/miners/custom/gminer
