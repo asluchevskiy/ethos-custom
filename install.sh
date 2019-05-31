84262565412289daba0b364e74d4d459d53b79e7
unrealjke <35392833+unrealjke@users.noreply.github.com>
2019-05-31 19:43:24 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 disallow
 minestop
 sleep 5
-TMP_DIR=/tmp
+TMP_DIR=/tmp/ethos-custom
+mkdir $TMP_DIR
+wget -q http://security.ubuntu.com/ubuntu/pool/main/g/gcc-5/gcc-5-base_5.4.0-6ubuntu1~16.04.10_amd64.deb -P $TMP_DIR
+wget -q http://security.ubuntu.com/ubuntu/pool/main/g/gcc-5/libstdc++6_5.4.0-6ubuntu1~16.04.10_amd64.deb -P $TMP_DIR
+sudo dpkg -i $TMP_DIR/*.deb
 git -C $TMP_DIR clone https://github.com/unrealjke/ethos-custom.git
 sudo cp -r $TMP_DIR/ethos-custom/opt/* /opt/
 sudo chown 1000:1000 /opt/miners/custom
