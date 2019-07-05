9951a45754755752c0b586350e06c913c3a5910b
unrealjke <35392833+unrealjke@users.noreply.github.com>
2019-07-05 11:00:34 +0400

    Update gminer.sh

--- gminer.sh
+++ gminer.sh
@@ -10,8 +10,8 @@ wget -q https://github.com/develsoftware/GMinerRelease/releases/download/$VER/gm
 rm /opt/miners/custom/custom
 rm -rf /opt/miners/custom/gminer
 mkdir /opt/miners/custom/gminer
-tar -xvf gmine_$VERSION\_linux64.tar.xz -C /opt/miners/custom/gminer
-rm -rf gmine_*
+tar -xvf gminer_$VERSION\_linux64.tar.xz -C /opt/miners/custom/gminer
+rm -rf gminer_*
 ln -s /opt/miners/custom/gminer/miner /opt/miners/custom/custom
 echo 'custom gminer-'$VERSION > /opt/miners/custom/custom-version.txt
 sudo service ethos-miner-monitor restart
