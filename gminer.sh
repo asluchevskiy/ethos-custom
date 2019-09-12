466202fd004119528b1a9555c935a24f41e44176
unrealjke <35392833+unrealjke@users.noreply.github.com>
2019-09-12 12:02:01 +0400

    Update gminer.sh

--- gminer.sh
+++ gminer.sh
@@ -1,19 +1,27 @@
-VERSION='1_62'
-VER='1.62'
+VERSION=''
+
+if [ -z "$VERSION" ];
+then
+        GET='https://github.com'$(curl -Ls https://github.com/develsoftware/GMinerRelease/releases/latest | grep linux | sed -n 's/.*href="\([^"]*\).*/\1/p')
+        VERSION=$(echo $GET | grep -Po '\d_\d*')
+else
+        GET='https://github.com/develsoftware/GMinerRelease/releases/download/'$VERSION'/gminer_'${VERSION/./_}'_linux64.tar.xz'
+
+fi
+
 disallow
 minestop
-echo 'waiting for 15 seconds before continue'
-sleep 15
+echo 'waiting for 5 seconds before continue'
+sleep 5
 curl -s https://raw.githubusercontent.com/unrealjke/ethos-custom/master/opt/miners/custom/hash-monitor > /opt/miners/custom/hash-monitor
 cd /tmp
-wget -q https://github.com/develsoftware/GMinerRelease/releases/download/$VER/gminer_$VERSION\_linux64.tar.xz
+wget -q $GET
 rm /opt/miners/custom/custom
 rm -rf /opt/miners/custom/gminer
 mkdir /opt/miners/custom/gminer
-tar -xvf gminer_$VERSION\_linux64.tar.xz -C /opt/miners/custom/gminer
+tar -xvf gminer_${VERSION/./_}_linux64.tar.xz -C /opt/miners/custom/gminer
 rm -rf gminer_*
 ln -s /opt/miners/custom/gminer/miner /opt/miners/custom/custom
 echo 'custom gminer-'$VERSION > /opt/miners/custom/custom-version.txt
 sudo service ethos-miner-monitor restart
 allow
-echo 'done'
