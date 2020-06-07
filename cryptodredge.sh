c1201bb08650406535de571323fed5a111eab923
unrealjke <35392833+unrealjke@users.noreply.github.com>
2020-06-07 21:02:31 +0400

    Update cryptodredge.sh

--- cryptodredge.sh
+++ cryptodredge.sh
@@ -2,9 +2,9 @@ VERSION='0.22.0'
 
 disallow
 minestop
-echo 'waiting for 15 seconds before continue'
-sleep 15
-curl -s https://raw.githubusercontent.com/unrealjke/ethos-custom/master/opt/miners/custom/hash-monitor > /opt/miners/cu$
+echo 'waiting for 5 seconds before continue'
+sleep 5
+curl -s https://raw.githubusercontent.com/unrealjke/ethos-custom/master/opt/miners/custom/hash-monitor > /opt/miners/custom/hash-monitor
 cd /tmp
 wget -q https://github.com/technobyl/CryptoDredge/releases/download/v$VERSION/CryptoDredge_$VERSION\_cuda_10.0_linux.tar.gz
 rm -rf /opt/miners/custom/cryptodredge
