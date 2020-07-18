45010d76677c53ed5e9fb8f79ec38a26a778ddd8
Arseniy Sluchevskiy <arseniy.sluchevskiy@gmail.com>
2020-07-18 15:45:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ minestop
 sleep 5
 TMP_DIR=/tmp/ethos-custom
 mkdir $TMP_DIR
-wget -q http://security.ubuntu.com/ubuntu/pool/main/g/gcc-5/gcc-5-base_5.4.0-6ubuntu1~16.04.10_amd64.deb -P $TMP_DIR
-wget -q http://security.ubuntu.com/ubuntu/pool/main/g/gcc-5/libstdc++6_5.4.0-6ubuntu1~16.04.10_amd64.deb -P $TMP_DIR
+wget -q http://security.ubuntu.com/ubuntu/pool/main/g/gcc-5/gcc-5-base_5.4.0-6ubuntu1~16.04.12_amd64.deb -P $TMP_DIR
+wget -q http://ge.archive.ubuntu.com/ubuntu/pool/main/g/gcc-5/libstdc++6_5.4.0-6ubuntu1~16.04.12_amd64.deb -P $TMP_DIR
 sudo dpkg -i $TMP_DIR/*.deb
 git -C $TMP_DIR clone https://github.com/unrealjke/ethos-custom.git
 sudo cp -r $TMP_DIR/ethos-custom/opt/* /opt/
