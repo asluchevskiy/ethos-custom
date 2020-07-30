b3de3632c550e6a9a7f354f0b2e1f69952084c3c
Arseniy Sluchevskiy <arseniy.sluchevskiy@gmail.com>
2020-07-30 13:07:47 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir $TMP_DIR
 wget -q http://security.ubuntu.com/ubuntu/pool/main/g/gcc-5/gcc-5-base_5.4.0-6ubuntu1~16.04.12_amd64.deb -P $TMP_DIR
 wget -q http://ge.archive.ubuntu.com/ubuntu/pool/main/g/gcc-5/libstdc++6_5.4.0-6ubuntu1~16.04.12_amd64.deb -P $TMP_DIR
 sudo dpkg -i $TMP_DIR/*.deb
-git -C $TMP_DIR clone https://github.com/unrealjke/ethos-custom.git
+git -C $TMP_DIR clone https://github.com/asluchevskiy/ethos-custom.git
 sudo cp -r $TMP_DIR/ethos-custom/opt/* /opt/
 sudo chown 1000:1000 /opt/miners/custom
 sudo chown 1000:1000 /opt/miners/custom/*
