ade171ea6cdda9166f58c72ada1a4d7c000094f6
unrealjke <35392833+unrealjke@users.noreply.github.com>
2021-02-10 12:41:33 +0400

    Create lolminer.sh

---
+++ lolminer.sh
@@ -0,0 +1,23 @@
+VERSION='1.23'
+
+disallow
+minestop
+echo 'waiting for 5 seconds before continue'
+sleep 5
+curl -s https://raw.githubusercontent.com/unrealjke/ethos-custom/master/opt/miners/custom/hash-monitor > /opt/miners/custom/hash-monitor
+cd /tmp
+rm -rf lolMiner*
+rm -rf $VERSION
+wget -q https://github.com/Lolliedieb/lolMiner-releases/releases/download/$VERSION/lolMiner_v$VERSION\_Lin64.tar.gz
+rm /opt/miners/custom/custom
+rm -rf /opt/miners/custom/lolminer
+mkdir /opt/miners/custom/lolminer
+tar -xvf lolMiner*
+mv ./$VERSION/* /opt/miners/custom/lolminer
+rm -rf lolMiner*
+rm -rf $VERSION
+ln -s /opt/miners/custom/lolminer/lolMiner /opt/miners/custom/custom
+chmod +x /opt/miners/custom/custom
+echo 'custom lolminer-'$VERSION > /opt/miners/custom/custom-version.txt
+sudo service ethos-miner-monitor restart
+allow
