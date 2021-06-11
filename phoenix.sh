a1219e4952585248894a2a490e4b596e58e4810d
unrealjke <35392833+unrealjke@users.noreply.github.com>
2021-06-11 09:42:26 +0400

    Update phoenix.sh

--- phoenix.sh
+++ phoenix.sh
@@ -1,4 +1,4 @@
-VERSION='5.0b'
+VERSION='5.6d'
 
 disallow
 minestop
@@ -7,11 +7,13 @@ sleep 5
 curl -s https://raw.githubusercontent.com/unrealjke/ethos-custom/master/opt/miners/custom/hash-monitor > /opt/miners/custom/hash-monitor
 cd /tmp
 rm -rf PhoenixMiner*
-wget -q https://github.com/Phoenix-Miner/PhoenixMiner/releases/download/$VERSION/PhoenixMiner_$VERSION\_Linux.zip
+wget -q https://phoenixminer.info/downloads/PhoenixMiner_$VERSION\_Linux.tar.gz
+#wget -q https://github.com/Phoenix-Miner/PhoenixMiner/releases/download/$VERSION/PhoenixMiner_$VERSION\_Linux.zip
 rm /opt/miners/custom/custom
 rm -rf /opt/miners/custom/phoenix
 mkdir /opt/miners/custom/phoenix
-unzip PhoenixMiner*
+#unzip PhoenixMiner*
+tar -xvf PhoenixMiner*
 mv ./PhoenixMiner_$VERSION\_Linux/* /opt/miners/custom/phoenix
 rm -rf PhoenixMiner*
 ln -s /opt/miners/custom/phoenix/PhoenixMiner /opt/miners/custom/custom
