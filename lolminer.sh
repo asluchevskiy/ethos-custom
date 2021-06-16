bb8d14d7085a4cbcc84d7060d20a4f1321368a98
unrealjke <35392833+unrealjke@users.noreply.github.com>
2021-06-16 14:58:56 +0400

    Update lolminer.sh

--- lolminer.sh
+++ lolminer.sh
@@ -1,4 +1,4 @@
-VERSION='5.6d'
+VERSION='1.23'
 
 disallow
 minestop
@@ -6,18 +6,16 @@ echo 'waiting for 5 seconds before continue'
 sleep 5
 curl -s https://raw.githubusercontent.com/unrealjke/ethos-custom/master/opt/miners/custom/hash-monitor > /opt/miners/custom/hash-monitor
 cd /tmp
-rm -rf PhoenixMiner*
-wget -q https://phoenixminer.info/downloads/PhoenixMiner_$VERSION\_Linux.tar.gz
-#wget -q https://github.com/Phoenix-Miner/PhoenixMiner/releases/download/$VERSION/PhoenixMiner_$VERSION\_Linux.zip
+rm -rf lolMiner*
+wget -q https://github.com/Lolliedieb/lolMiner-releases/releases/download/$VERSION/lolMiner_v$VERSION\_Lin64.tar.gz
 rm /opt/miners/custom/custom
-rm -rf /opt/miners/custom/phoenix
-mkdir /opt/miners/custom/phoenix
-#unzip PhoenixMiner*
-tar -xvf PhoenixMiner*
-mv ./PhoenixMiner_$VERSION\_Linux/* /opt/miners/custom/phoenix
-rm -rf PhoenixMiner*
-ln -s /opt/miners/custom/phoenix/PhoenixMiner /opt/miners/custom/custom
+rm -rf /opt/miners/custom/lolminer
+mkdir /opt/miners/custom/lolminer
+tar -xvf lolMiner*
+mv ./$VERSION/* /opt/miners/custom/lolminer
+rm -rf lolMiner*
+ln -s /opt/miners/custom/lolminer/lolMiner /opt/miners/custom/custom
 chmod +x /opt/miners/custom/custom
-echo 'custom phoenix-'$VERSION > /opt/miners/custom/custom-version.txt
+echo 'custom lolminer-'$VERSION > /opt/miners/custom/custom-version.txt
 sudo service ethos-miner-monitor restart
 allow
