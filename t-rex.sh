e64e663f66cbf5a5a11be9abfb02a5314a21c0f3
unrealjke <35392833+unrealjke@users.noreply.github.com>
2022-09-15 12:24:48 +0400

    Update t-rex.sh

--- t-rex.sh
+++ t-rex.sh
@@ -1,17 +1,17 @@
 disallow
 minestop
 echo 'waiting for 15 seconds before continue'
-sleep 15
+sleep 1
 curl -s https://raw.githubusercontent.com/unrealjke/ethos-custom/master/opt/miners/custom/hash-monitor > /opt/miners/custom/hash-monitor
 cd /tmp
-wget -q https://github.com/trexminer/T-Rex/releases/download/0.24.8/t-rex-0.24.8-linux.tar.gz
+wget -q https://github.com/trexminer/T-Rex/releases/download/0.18.9/t-rex-0.18.9-linux-cuda9.2.tar.gz
 rm /opt/miners/custom/custom
 rm -rf /opt/miners/custom/t-rex
 mkdir /opt/miners/custom/t-rex
-tar -xzf t-rex-0.24.8-linux.tar.gz -C /opt/miners/custom/t-rex
-rm t-rex-0.24.8-linux.tar.gz
+tar -xzf t-rex-0.18.9-linux-cuda9.2.tar.gz -C /opt/miners/custom/t-rex
+rm t-rex-0.18.9-linux-cuda9.2.tar.gz
 ln -s /opt/miners/custom/t-rex/t-rex /opt/miners/custom/custom
-echo 'custom trex-0.24.8' > /opt/miners/custom/custom-version.txt
+echo 'custom trex-0.18.9' > /opt/miners/custom/custom-version.txt
 sudo service ethos-miner-monitor restart
 allow
 echo 'done'
